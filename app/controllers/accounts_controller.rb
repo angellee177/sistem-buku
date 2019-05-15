e144e87c83a92cf4a153bd2a9e69e6418ec9cf43
angellee <angelriapurnamasari17@gmail.com>
2019-05-15 17:21:55 +0700

    menambahkan link ke Menu Login

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,7 +6,6 @@ class AccountsController < ApplicationController
     def create
         user = User.new(user_params)
         user.save
-        redirect_to new_session_path
     end
     
     private
